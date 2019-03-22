341fbeeaac3aa14d783a2528c503e4abc0eb4824
sheryl-ai <sheryl.zhangxi@gmail.com>
2019-03-22 15:05:56 -0400

    change variable names

--- demo.py
+++ demo.py
@@ -40,15 +40,15 @@ def load_weights(num, train_label=None):
     return wgt_proba
 
 
-def prediction_demo(train_x, train_label, gene, variation):
-    wgt_proba = load_weights(len(train_x), None)
-    for idx, row in train_x.iterrows():
+def prediction_demo(stage1_x, stage1_label, gene, variation):
+    wgt_proba = load_weights(len(stage1_x), None)
+    for idx, row in stage1_x.iterrows():
         if gene == row['Gene'] and variation == row['Variation']:
             pred = wgt_proba[row['ID']]
             print('ID:', row['ID'], 'Gene:', row['Gene'], 'Variation:', row['Variation'])
 
             gt = np.zeros(9)
-            gt[train_label[row['ID']]-1] = 1
+            gt[stage1_label[row['ID']]-1] = 1
             print('{:^10} {:^20} {:<20}'.format('class', 'prediction', 'groundtruth'))
             for cls in range(9):
                 print('{:^10} {:<25} {:<3}'.format(cls+1, pred[cls], gt[cls]))
@@ -60,20 +60,20 @@ def prediction_demo(train_x, train_label, gene, variation):
 
 if __name__ == '__main__':
     # load nips stage1 train and test data
-    train_variant = pd.read_csv('data/stage1_variants')
-    test_variant = pd.read_csv('data/stage2_variants')
+    stage1_variant = pd.read_csv('data/stage1_variants')
+    stage2_variant = pd.read_csv('data/stage2_variants')
 
-    # load filter oncokb data from nips stage1 test data
+    # load stage1_solution data
     stage1_solution = pd.read_csv('data/stage1_solution_filtered.csv')
-    stage1_solution_variant = test_variant.loc[stage1_solution['ID']]
+    stage1_solution_variant = stage2_variant.loc[stage1_solution['ID']]
     stage1_solution_variant['ID'] = [id + 3321 for id in range(stage1_solution.shape[0])]
     labels = list(stage1_solution[stage1_solution != 0].drop(['ID'], axis=1).stack().index)
     stage1_solution_variant['Class'] = pd.Series(data=[int(val[1][-1]) for val in labels], index=stage1_solution['ID'])
 
-    # merge nips stage1 and oncokb data as training set
-    train = pd.concat((train_variant, stage1_solution_variant), axis=0, ignore_index=True)
-    train_label = train['Class'].values
-    train_x = train.drop(['Class'], axis=1)
+    # merge nips stage1 and stage1_solution
+    stage1 = pd.concat((stage1_variant, stage1_solution_variant), axis=0, ignore_index=True)
+    stage1_label = stage1['Class'].values
+    stage1_x = stage1.drop(['Class'], axis=1)
 
     parser = argparse.ArgumentParser(description='demo.py: get predicted probability for given (gene, variation) pair')
     parser.add_argument('-gene', help='gene name')
@@ -83,9 +83,4 @@ if __name__ == '__main__':
     print()
 
     # demo for prediction given (gene, variation)
-    ## examples
-    # pred = prediction_demo(train_x, train_label, 'DICER1', 'G1809K')    # check log loss on train + valid
-    # pred = prediction_demo(train_x, gene='DICER1', variation='G1809K')  # get prediction only via (gene, variation)
-    # for gene, var in zip(train['Gene'], train['Variation']):
-    #     prediction_demo(train_x, train_label, gene=gene, variation=var)
-    pred = prediction_demo(train_x, train_label, gene=args.gene, variation=args.variation)
+    pred = prediction_demo(stage1_x, stage1_label, gene=args.gene, variation=args.variation)
